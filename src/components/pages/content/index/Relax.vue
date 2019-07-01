ef9e81237afd1ff0af8fd06967572a37009260a8
PlayWay <dim.art2011@yandex.ru>
2019-07-01 21:58:30 +0300

    change some settings

--- src/components/pages/content/index/Relax.vue
+++ src/components/pages/content/index/Relax.vue
@@ -8,7 +8,7 @@
                 +e.content
                     +e.background
                         +e.img--family
-                            parallax-element.background-image(:parallaxStrength="10" :type="'depth'")
+                            parallax-element.background-image(:parallaxStrength="-0.3" :type="'depth'")
                                 img(src="public/media/images/relax/bg.png")
                     +e.text--noImage
                         p Это хорошая возможность провести незабываемое время вместе с близкими. Восстановить силы и посетить значимые места Паттайи
@@ -17,19 +17,19 @@
                             span Будьте здоровы
                             p Наслаждайтесь вкуснейшими тропическими фруктами и коктейлями из свежего манго, которые помогают вашему организму провести полную детоксикацию
                             +e.img--pineapple.--absolute
-                                parallax-element.background-image(:parallaxStrength="-3" :type="'translation'")
+                                parallax-element.background-image(:parallaxStrength="-0.5" :type="'translation'")
                                     img(src="public/media/images/relax/pineapple.svg")
                             +e.img--apple.--absolute
-                                parallax-element.background-image(:parallaxStrength="5" :type="'translation'")
+                                parallax-element.background-image(:parallaxStrength="0.5" :type="'translation'")
                                     img(src="public/media/images/relax/apple.svg")
                         +e.text--banana
                             span Наслаждайтесь отдыхом
                             p На территории расположены SPA, спортивная площадка, бассейн и прочие приятные мелочи.
                             +e.img--juice.--absolute
-                                parallax-element.background-image(:parallaxStrength="3" :type="'translation'")
+                                parallax-element.background-image(:parallaxStrength="0.5" :type="'translation'")
                                     img(src="public/media/images/relax/juice.svg")
                             +e.img--banana.--absolute
-                                parallax-element.background-image(:parallaxStrength="-5" :type="'translation'")
+                                parallax-element.background-image(:parallaxStrength="-0.5" :type="'translation'")
                                     img(src="public/media/images/relax/banana.svg")
 </template>
 
